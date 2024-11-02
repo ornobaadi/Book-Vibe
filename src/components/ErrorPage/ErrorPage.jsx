a5038088a1331488c6658463c0b2d788cf50807c
ornobaadi <aadi4789@gmail.com>
2024-11-02 18:11:49 +0600

    Banner and Fetched data

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text5xl">Page not found</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import NavBar from "../NavBar/NavBar";
 
 const Root = () => {
     return (
-        <div>
-            <h3>Hello from Root</h3>
+        <div className="max-w-6xl mx-auto">
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
